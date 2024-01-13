94a5291c200d73a58536511a8eeeaec1d9211ba4
Constanza Mora <constanza.morasan@gmail.com>
2024-01-13 01:56:48 +0100

    Added API part 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "0t02509c5159bfa0f925o3abcad4776b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#weather-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
