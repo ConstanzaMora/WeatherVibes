c4184ff148e255755170d26ceb9ec250d9b7615d
Constanza Mora <constanza.morasan@gmail.com>
2024-01-14 03:00:44 +0100

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -3,11 +3,44 @@ function refreshWeather(response) {
 
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#weather-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  let sensationElement = document.querySelector("#thermal-sensation");
+  let sensation = response.data.temperature.feels_like;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  console.log(response.data);
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
   temperatureElement.innerHTML = Math.round(temperature);
+  windElement.innerHTML = response.data.wind.speed;
+  sensationElement.innerHTML = Math.round(sensation);
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thusday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
 function searchCity(city) {
   let apiKey = "0t02509c5159bfa0f925o3abcad4776b";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
@@ -25,4 +58,4 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Mexico City");
+searchCity("Dresden");
